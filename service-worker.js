1dd68dc144490a29031e149a71b971e29aa0f231
yano <yano.naoki@intra-mart.jp>
2015-06-26 10:28:50 +0900

    .

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
-self.addEventListener('push', function(event) {  
+self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  var title = 'Yay a message.';  
-  var body = 'We have received a push message.';  
-  var icon = '/images/icon-192x192.png';  
-  var tag = 'simple-push-demo-notification-tag';
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = 'images/icon-192x192.png';
 
-  event.waitUntil(  
-    self.registration.showNotification(title, {  
-      body: body,  
-      icon: icon,  
-      tag: tag  
-    })  
-  );  
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon
+    })
+  );
 });
